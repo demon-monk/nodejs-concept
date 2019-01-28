fe0bad2db5cd36e5bc53d2b4dbb8c21dfebd1b78
demon-monk <antdude@126.com>
2019-01-28 17:16:07 +0800

    feat: 🎸 show how to use cluster to run multiple node instances

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-const express = require("express");
-const app = express();
-function doWork(duration) {
-  const startTime = Date.now();
-  while (Date.now() - startTime < duration) {}
+const cluster = require("cluster");
+if (cluster.isMaster) {
+  cluster.fork();
+  cluster.fork();
+} else {
+  const express = require("express");
+  const app = express();
+  function doWork(duration) {
+    const startTime = Date.now();
+    while (Date.now() - startTime < duration) {}
+  }
+  app.get("/", (req, res) => {
+    doWork(5000);
+    res.send("Hi there!");
+  });
+  app.get("/fast", (req, res) => {
+    res.send("this is fast");
+  });
+  app.listen(3000);
 }
-app.get("/", (req, res) => {
-  doWork(5000);
-  res.send("Hi there!");
-});
-app.listen(3000);
