cc63650e684d4e0e587708453f01145143f2a544
demon-monk <antdude@126.com>
2019-01-28 18:37:00 +0800

    feat: 🎸 demenstrate how increasing forks affects performance

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require("cluster");
+const crypto = require("crypto");
+
+function doWork(fn) {
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    fn();
+  });
+}
+
 if (cluster.isMaster) {
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
   cluster.fork();
   cluster.fork();
 } else {
   const express = require("express");
   const app = express();
-  function doWork(duration) {
-    const startTime = Date.now();
-    while (Date.now() - startTime < duration) {}
-  }
   app.get("/", (req, res) => {
-    doWork(5000);
-    res.send("Hi there!");
+    doWork(() => {
+      res.send("Hi there!");
+    });
   });
   app.get("/fast", (req, res) => {
     res.send("this is fast");
