1e207bad868dea5879357ea27951f80852fe2c04
Bang Zhang <bang.zhang@liulishuo.com>
2019-01-28 14:46:04 +0800

    feat: 🎸 change threadPool size

--- src/thread.js
+++ src/thread.js
@@ -1,8 +1,9 @@
+process.env.UV_THREADPOOL_SIZE = 5
 const crypto = require('crypto')
 const startTime = Date.now()
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    console.log('1:', Date.now() - startTime)
-})
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    console.log('2:', Date.now() - startTime)
-})
+
+for (let i = 0; i < 5; i++) {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        console.log(`${i+1}:`, Date.now() - startTime)
+    })
+}
